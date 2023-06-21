988ab53c126a07215153adad80a6f04c37a2d56e
Ranjan Verma <verma.ranjan62@gmail.com>
2023-06-21 16:27:30 +0530

    A Simple Counter App using Redux Toolkit

--- react_redux_beginner/src/App.jsx
+++ react_redux_beginner/src/App.jsx
@@ -1,4 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { actions } from './store/index'
+
 import './App.css'
 
 function App() {
@@ -6,15 +8,15 @@ function App() {
   const dispatch = useDispatch()
 
   const increment = () => {
-    dispatch({ type: 'INC' })
+    dispatch(actions.increment())
   }
 
   const decrement = () => {
-    dispatch({ type: 'DEC' })
+    dispatch(actions.decrement())
   }
 
   const addBy = () => {
-    dispatch({ type: 'ADD', payload: 10 })
+    dispatch(actions.addBy(10))
   }
   return (
     <div>

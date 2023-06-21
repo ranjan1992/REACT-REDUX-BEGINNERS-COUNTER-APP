28838e9146f89f9088838fef841e6cb2bbd58170
Ranjan Verma <verma.ranjan62@gmail.com>
2023-06-21 13:47:04 +0530

    Simple Counter App without Redux Toolkit (Old Way)

---
+++ react_redux_beginner/src/App.jsx
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux'
+import './App.css'
+
+function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
+  const increment = () => {
+    dispatch({ type: 'INC' })
+  }
+
+  const decrement = () => {
+    dispatch({ type: 'DEC' })
+  }
+
+  const addBy = () => {
+    dispatch({ type: 'ADD', payload: 10 })
+  }
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add By 10</button>
+    </div>
+  )
+}
+
+export default App
